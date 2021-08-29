fa1b5cf4170a254f21ecccdf81e1d28f517f984d
SpeedRacer <robertgeorge2@gmail.com>
2021-08-29 11:55:33 -0400

    Removed _id from User and Book. WIll use username and bookId as unique identifiers instead.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,7 +2,6 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type User {
-    _id: ID!
     userName: String!
     email: String!
     password: String!
@@ -10,9 +9,8 @@ const typeDefs = gql`
   }
 
   type Book {
-    _id: ID!
-    authors: String
-    description: String!
+    authors: [String]
+    description: String
     bookId: String!
     image: String
     link: String
@@ -20,15 +18,11 @@ const typeDefs = gql`
   }
 
   type Query {
-    users: [User]
-    user(_id: String): User
-    books: [Book]
-    book(_id: String): Book
     me: User
   }
 
   type Auth {
-    token: ID!
+    token: ID
     user: User
   }
 
@@ -36,9 +30,9 @@ const typeDefs = gql`
   
   module.exports = typeDefs;
   
-  // type Mutation {
-  //   addUser(username: String!, email: String!, password: String!): Auth
-  //   login(email: String!, password: String!): Auth
-  //   saveBook(_id: book.id, authors: String, description: String!, bookId: String!, image: String, link: String, title: String!): User
-  //   removeBook(_id: user.id, _id:book.id): User
-  // }
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    // saveBook(authors: String, description: String!, bookId: String!, image: String, link: String, title: String!): User
+    // removeBook(_id: user.id, _id:book.id): User
+  }
